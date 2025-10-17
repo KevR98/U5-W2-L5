a0dee8d58ea213962b59c4e28f5b0fe504fccd9d
KevR98 <ramil.kevin98@gmail.com>
2025-10-17 17:02:43 +0200

    fixed

--- src/main/java/U5_W2_L5/Config/AppConfig.java
+++ src/main/java/U5_W2_L5/Config/AppConfig.java
@@ -12,13 +12,15 @@ import java.util.Map;
 public class CloudinaryConfig {
 
     @Bean
-    public Cloudinary uploader(@Value("${cloudinary.name}") String cloudName,
-                               @Value("${cloudinary.key}") String apiKey,
-                               @Value("${cloudinary.secret}") String apiSecret) {
-        Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", cloudName);
-        config.put("api_key", apiKey);
-        config.put("api_secret", apiSecret);
-        return new Cloudinary(config);
-    }
+//    public Cloudinary uploader(@Value("${cloudinary.name}") String cloudName,
+//                               @Value("${cloudinary.key}") String apiKey,
+//                               @Value("${cloudinary.secret}") String apiSecret) {
+//        Map<String, String> config = new HashMap<>();
+//        config.put("cloud_name", cloudName);
+//        config.put("api_key", apiKey);
+//        config.put("api_secret", apiSecret);
+//        return new Cloudinary(config);
+//    }
+
+    // mi da errore
 }
